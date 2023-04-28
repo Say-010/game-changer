8059c12bbb805479642d7999d38ba98fef211c6f
unknown <sagarsahu0110@gmail.com>
2023-04-28 14:57:17 +0530

    eritten by me

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import GuessLuckyNumber from './component/GuessLuckyNumber';
+
+function App() {
+  return (
+    <div className="App">
+
+     <GuessLuckyNumber/> 
+
+    </div>
+  );
+}
+
+export default App;
